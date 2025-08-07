27db1158e22832699b64cad46bb5b39df2494d84
Wallysom-fer <wallysom.f@escolar.ifrn.edu.br>
2025-08-07 01:18:55 -0300

    Corrigindo erros

--- voo_app/domain/entities/bagagem.py
+++ voo_app/domain/entities/bagagem.py
@@ -2,5 +2,6 @@ class Bagagem:
     def __init__(self, descricao: str, peso: float):
         self.descricao = descricao
         self.peso = peso
+        
     def __str__(self):
         return f"{self.descricao} – {self.peso} kg"
--- voo_app/domain/entities/passageiro.py
+++ voo_app/domain/entities/passageiro.py
@@ -2,16 +2,20 @@ from domain.entities.pessoa import Pessoa
 from domain.entities.bagagem import Bagagem
 
 class Passageiro(Pessoa):
-    def __init__(self, nome:str, cpf:str, bagagens: Bagagem):
+    def __init__(self, nome: str, cpf: str, bagagens: list[Bagagem] = None):
         super().__init__(nome, cpf)
         self.bagagens = bagagens if bagagens is not None else []
 
-    def adicionar_bagagens(self, descricao:str):
-        self.bagagens.append(descricao)
-    
+    def adicionar_bagagem(self, descricao: str, peso: float):
+        nova_bagagem = Bagagem(descricao, peso)
+        self.bagagens.append(nova_bagagem)
+
     def listar_bagagens(self):
-        for bagagem in self.bagagens:
-            print(bagagem)
-    
+        if not self.bagagens:
+            print("Nenhuma bagagem registrada.")
+        else:
+            for bagagem in self.bagagens:
+                print(bagagem)
+
     def __repr__(self):
         return f"<Passageiro nome={self.nome} cpf={self.cpf} bagagens={len(self.bagagens)}>"
